aae8dabc4c0d4ca5abf2dd01575482afd196a5ff
Ryan <ryanchristiancastillo0@gmail.com>
2026-09-14 20:45:35 +0800

    latest project bug updated 90%

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from '../pages/public/Home';
-import AdminLogin from '../pages/auth/Login';
-import JudgeLogin from '../pages/auth/JudgeLogin';
-import ForgotPassword from '../pages/auth/ForgotPassword';
+import AdminLogin from '../pages/Auth/Login';
+import JudgeLogin from '../pages/Auth/JudgeLogin';
+import ForgotPassword from '../pages/Auth/ForgotPassword';
 import CreateSchoolForm from '../pages/public/SchoolRegistration';
 import AdminRoutes, { ContestScope } from './AdminRoutes';
 import JudgeRoutes from './JudgeRoutes';
